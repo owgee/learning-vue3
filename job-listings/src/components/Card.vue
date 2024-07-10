0f969fc7b704b7e33c59bbf53a27d85a58112e9f
Owden Godson <owdeng@gmail.com>
2024-07-10 01:49:00 -0400

    add homecards with dynamic bg colors as props

---
+++ job-listings/src/components/Card.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  bg: String,
+  default: 'bg-gray-100'
+})
+</script>
+<template>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>

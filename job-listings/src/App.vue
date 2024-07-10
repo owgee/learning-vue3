cdb5394b9f2a076fbd9dab2ae38a5911f21b32bb
Owden Godson <owdeng@gmail.com>
2024-07-09 23:09:54 -0400

    add job listings using create vue command

---
+++ job-listings/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'OG',
+      status: 'pending',
+      tasks: ['Task one', 'Task two', 'Task three'],
+      link: 'https://google.com'
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending'
+      } else if (this.status === 'pending') {
+        this.status = 'active'
+      } else {
+        this.status = 'active'
+      }
+
+    }
+  }
+}
+</script>
+<template>
+  <h1> {{ name }}</h1>
+  <p v-if="status=='active'"> User is active</p>
+  <p v-else> User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <a :href="link">Go to Google</a>
+  <button @click="toggleStatus">Change status</button>
+</template>
